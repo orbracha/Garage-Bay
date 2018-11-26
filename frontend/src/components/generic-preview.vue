182c2312db4b52894a4fe850282583043d318591
Or Bracha <orbracha2308@gmail.com>
2018-11-26 19:43:23 +0200

    chat

--- frontend/src/components/generic-preview.vue
+++ frontend/src/components/generic-preview.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <img src="https://api.adorable.io/avatars/285/abott@adorable.png">
-    <h1 >{{item.from.nickname}}</h1>
+    <h1>{{item.from.nickname}}</h1>
     <p>{{item.txt}}</p>
   </li>
 </template>
@@ -10,6 +10,25 @@
   li{
     list-style-type: none;
     display: grid;
+    grid-template-columns: 50px 1fr;
+    grid-template-rows: 50px;
+    img {
+      height: 50px;
+      width: 50px;
+      grid-column: 1/2;
+      grid-row: 1/3;
+    }
+    h1{
+      grid-column-start: 2;
+      text-align: left;
+      margin: 0;
+    }
+    p{
+      grid-column-start: 2;
+      grid-row-start: 2;
+      text-align: left;
+      margin: 0;
+    }
   }
 </style>
 
