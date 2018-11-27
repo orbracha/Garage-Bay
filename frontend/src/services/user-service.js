ffe05853c32230859a948de36b866c87459526cc
Yardenb <yardenesh@gmail.com>
2018-11-27 13:50:31 +0200

    item-details updated

--- frontend/src/services/user-service.js
+++ frontend/src/services/user-service.js
@@ -2,13 +2,13 @@
 
 
 
-var users = [_createUser('avi bobi', '1234', '123', 'user.jpg' ,["9cO","thA","N2E","2KH","b1w",]),
-_createUser('yarden hachla', '4343', '456', 'user.jpg',["sEk","2cN","zYJ","NRt","Rpd","OMt","RxJ", "yiy","MXW"]),
-_createUser('elad yogurt', '5675', '678', 'user.jpg',["gPE","V6f","45z","cV8","gIp","GWG"])];
+var users = [_createUser('avi bobi', '1234', '123', 'https://i.stack.imgur.com/l60Hf.png' ,["9cO","thA","N2E","2KH","b1w",]),
+_createUser('yarden hachla', '4343', '456', 'https://i.stack.imgur.com/l60Hf.png',["sEk","2cN","zYJ","NRt","Rpd","OMt","RxJ", "yiy","MXW"]),
+_createUser('elad yogurt', '5675', '678', 'https://i.stack.imgur.com/l60Hf.png',["gPE","V6f","45z","cV8","gIp","GWG"])];
 
 
 function getById(userId) {
-    user = users.find(user => user._id === userId)
+    var user = users.find(user => user._id === userId)
     return Promise.resolve(user);
 }
 
--- frontend/src/views/item-details.vue
+++ frontend/src/views/item-details.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="item-container">
-    {{currItem}}
-    <header>
-      <span slot="leftArrow">
-        <b>←</b>
-      </span>
-      <div class="content" slot="headline">
-        <h3>{{currItem.title}}</h3>
-        <span>Created at: {{currItem.createAt}}</span>
-      </div>
-      <span slot="optionalIcon">❤</span>
-    </header>
+    <div v-if="!isLoaded">Loading...</div>
+    <div else>
+      <header>
+        <span slot="leftArrow">
+          <b>←</b>
+        </span>
+        <div class="content" slot="headline">
+          <h3>{{currItem.title}}</h3>
+          <span>Created at: {{currItem.createAt}}</span>
+        </div>
+        <span slot="optionalIcon">❤</span>
+      </header>
 
-    <!-- {{currItem.img}} -->
-    <img src="http://dummyimage.com/1200x800.png" alt="placeholder image">
-    <div class="chatLink-container">
-      <span @click="chatClicked">&#9993;</span>
-      <p>Like it? Start chat</p>
-    </div>
-    <div class="details-container">
-      <p>Description: {{currItem.desc}}</p>
-      <p>Location: need to add with geo location</p>
-      <p>Condition: {{currItem.condition}}</p>
-      <div class="seller-details">
-        <img class="seller-img" src="http://dummyimage.com/50x50.png" alt="placeholder image">
-        <p>Seller Name</p>
-        <p>Seller rate</p>
-        <p>How many current items</p>
+      <img :src="currItem.img" alt="placeholder image">
+      <div class="chatLink-container">
+        <span @click="chatClicked" >&#9993;</span>
+        <p>Like it? Start chat</p>
       </div>
-      <google-map />
+
+      <div class="details-container">
+        <p>Description: {{currItem.desc}}</p>
+        <p>Location: ***need to add***</p>
+        <p>Condition: {{currItem.condition}}</p>
+        <div class="seller-details flexSet">
+          <img class="seller-img" :src="currSeller.img" alt="placeholder image">
+          <div>
+            <p>{{currSeller.nickname}} Seller rate:***need to add***</p>
+            <p>Currently selling {{currSeller.itemList.length}} items</p>
+          </div>
+        </div>
+        {{currSeller}}
+        <google-map/>
+      </div>
     </div>
   </div>
 </template>
@@ -36,69 +40,87 @@
 <script>
 import GoogleMap from "@/components/GoogleMap";
 export default {
-    name: 'item-details',
-    components:{
-        GoogleMap
-    },
-    data(){
-        return{
-            currItem:{}
-        }
-    },
-    created(){
-        var itemId=this.$route.params.id
-        this.$store.dispatch({type: 'getItemById',itemId})
-            .then(item=>this.currItem=item)
-            //TODO WITH REAL DATA: 
-            //after getting the item - get seller id=> set seller name, image, rate, how many current items
-    },
-    methods:{
-        chatClicked(){
-            console.log('chat link clicked');
-            
-        }
+  name: "item-details",
+  components: {
+    GoogleMap
+  },
+  data() {
+    return {
+      currItem: {},
+      currSeller: {},
+      isLoaded: false
+    };
+  },
+  created() {
+    var itemId = this.$route.params.id;
+    this.$store.dispatch({ type: "getItemById", itemId }).then(item => {
+      this.currItem = item;
+      var userId = this.currItem.sellerId;
+      this.$store.dispatch({ type: "getUserById", userId }).then(user => {
+        this.currSeller = user;
+        this.isLoaded = true;
+      });
+    });
+    //TODO WITH REAL DATA:
+    //after getting the item - get seller id=> set seller name, image, rate, how many current items
+  },
+  methods: {
+    chatClicked() {
+      console.log("chat link clicked");
+      this.$router.push(`/user/${this.currSeller._id}`)
     }
-}
+  },
+  computed: {
+    imgSrc() {
+      this.currSeller.img;
+    }
+  }
+};
 </script>
 
 <style>
-*{
-    box-sizing: border-box;
+* {
+  box-sizing: border-box;
 }
-.details-container{
-    margin: 10px 0;
-    text-align: left;
+.details-container {
+  margin: 10px 0;
+  text-align: left;
 }
-.seller-img{
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin: 20px;
+.seller-img {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  margin: 20px;
 
-    object-fit: cover;
-    object-position: center right;
+  object-fit: cover;
+  object-position: center right;
 }
-.chatLink-container{
-    display: flex;
-    justify-content: center;
+.chatLink-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.chatLink-container span {
+  margin-right: 5px;
+}
+.item-container {
+  width: 90%;
+  margin: 0 auto;
+}
+header {
+  display: flex;
+  background-color: #666666;
+  color: #e8e8e8;
+  align-items: center;
+  justify-content: center;
+}
+img {
+  width: 100%;
+  height: auto;
+}
+.flexSet{
+    display:flex;
     align-items: center;
-}
-.chatLink-container span{
-    margin-right: 5px;
-}
-.item-container{
-    width: 90%;
-    margin: 0 auto;
-}
-header{
-    display: flex;
-    background-color: #666666;
-    color:#e8e8e8;
-    align-items: center;
-    justify-content: center;
-}
-img{
-    width: 100%;
-    height: auto;
 }
 </style>
