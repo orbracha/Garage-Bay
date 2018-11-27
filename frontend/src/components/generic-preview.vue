c2282cbcf9fc94e710f1dd5b4be6547a10a26333
Or Bracha <orbracha2308@gmail.com>
2018-11-27 10:07:49 +0200

    generic

--- frontend/src/components/generic-preview.vue
+++ frontend/src/components/generic-preview.vue
@@ -1,22 +1,31 @@
 <template>
   <li>
-    <img src="https://api.adorable.io/avatars/285/abott@adorable.png">
-    <h1>{{item.from.nickname}}</h1>
-    <p>{{item.txt}}</p>
+    <div id="img-container" :style="{background:'black'}"></div>
+    <!-- <img src="https://api.adorable.io/avatars/285/abott@adorable.png"> -->
+    <div>
+      <h1>avi</h1>
+      <p>{{item.txt}}</p>
+    </div>
   </li>
 </template>
 
 <style lang="scss" scoped>
   li{
     list-style-type: none;
-    display: grid;
-    grid-template-columns: 50px 1fr;
-    grid-template-rows: 50px;
-    img {
+    display: flex;
+    margin-bottom: 5px;
+    // img {
+    //   height: 50px;
+    //   width: 50px;
+    //   grid-column: 1/2;
+    //   grid-row: 1/3;
+    // }
+    #img-container{
       height: 50px;
       width: 50px;
-      grid-column: 1/2;
-      grid-row: 1/3;
+      border-radius: 50%;
+      // background: url('https://api.adorable.io/avatars/285/abott@adorable.png') no-repeat;
+      background-size: center;
     }
     h1{
       grid-column-start: 2;
