b861930a11156308895742250631ec644fa3fd0c
Yardenb <yardenesh@gmail.com>
2018-11-28 09:15:26 +0200

    footer added

---
+++ frontend/src/components/garage-footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="footer">
+        <router-link to="/">&#127968;</router-link>
+        <router-link to="/chat">&#9993;</router-link>
+        <router-link to="/">&plus;</router-link>
+        <router-link to="/">❤</router-link>
+        <router-link to="/">&#9786;</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'garage-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+.footer{
+    display:flex;
+    background-color: #666666;
+    color: #e8e8e8;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
