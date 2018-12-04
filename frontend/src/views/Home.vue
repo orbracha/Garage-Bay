b372553c6535530e7bdf5003951e47cc1409c28d
Ladek <eladozy@gmail.com>
2018-12-04 17:48:07 +0200

    new navbar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,34 +1,42 @@
 <template>
   <div v-if="isLoading">Loding...</div>
 
-  <div class="home container" v-else>
-    
+  <div class="home" v-else>
     <header class="home-header">
-      <div class="logo-text">dibs</div>
-      <!-- <router-link to="/dibs">
-        <div v-if="user" class="dibs-container">
-          <img class="dibs-logo" src="../assets/img/moneyWhite.svg">
-          <span v-if="user.dibs.length" class="notification">{{user.dibs.length}}</span>
-        </div>
-      </router-link> -->
-      <!-- <router-link to="/dibs/ansDibs">
-        <div v-if="user" class="dibs-container">
-          <i class="fas fa-shopping-basket"/>
-          <span v-if="countOfDibsAns" class="notification">{{countOfDibsAns}}</span>
-        </div>
-      </router-link> -->
-      <input type="text" placeholder=" Search item" >
+      <div class="burger">
+      <i class="fas fa-times" v-if="isMenu" @click="toggleMenu"></i>
+      <i class="fas fa-bars" v-else @click="toggleMenu"></i>
+      </div>
+
+      <router-link to="/">
+        <div class="logo-text">dibs</div>
+      </router-link>
+
+      <input type="text" placeholder=" Search item">
+
       <router-link to="/search">
         <i class="fas fa-search"/>
       </router-link>
+
+      <router-link class="footer-item-2 flex row" v-if="user" :to="'/user/'+user._id">
+        <i class="fas fa-user"></i>
+      </router-link>
+
+      <router-link class="footer-item-2 header-profile-icon" v-else to="/login">
+        <i class="fas fa-user"></i>
+      </router-link>
     </header>
 
-    <garage-carousel v-if="user" :user="user"/>
-    
+    <div class="carousel-container">
+      <div class="carousel-header">Highest rated sellers</div>
+      <garage-carousel v-if="user" :user="user"/>
+    </div>
+
     <section class="home-content">
       <item-list class="item-list" :items="items"/>
     </section>
-    <garage-footer/>
+    <menu-screen class="screen" :class="{ open: isMenu }" @click.native="toggleMenu"/>
+    <garage-footer class="footer" :class="{ open: isMenu }"/>
   </div>
 </template>
 
@@ -37,23 +45,33 @@
 import itemList from "@/components/item-list.vue";
 import garageFooter from "@/components/garage-footer.vue";
 import garageCarousel from "@/components/garage-carousel.vue";
+import menuScreen from "@/components/screen.vue";
 
 export default {
   name: "home",
   components: {
     itemList,
     garageFooter,
-    garageCarousel
+    garageCarousel,
+    menuScreen
   },
   data() {
     return {
-      isLoading: true
+      isLoading: true,
+      isMenu: false
     };
   },
   created() {
     this.$store.dispatch({ type: "loadItems" }).then(res => {
       this.isLoading = false;
     });
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenu = !this.isMenu;
+      console.log(this.isMenu);
+      
+    }
   },
   computed: {
     items() {
@@ -70,5 +88,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.VueCarousel-navigation-next {
+  margin-right: 10px;
+  color: rgba(255, 255, 255, 0.815) !important;
+  font-size: 1.5rem;
+}
+.VueCarousel-navigation-prev {
+  margin-left: 10px;
+  color: rgba(255, 255, 255, 0.815) !important;
+  font-size: 1.5rem;
+}
 </style>
