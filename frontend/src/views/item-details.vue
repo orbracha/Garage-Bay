337cb97e4d6b799fd5a99c0ef410cae09646aa94
Or Bracha <orbracha2308@gmail.com>
2018-12-04 12:16:09 +0200

    details-css

--- frontend/src/views/item-details.vue
+++ frontend/src/views/item-details.vue
@@ -8,39 +8,61 @@
           <span>Created at: {{currItem.createAt}})</span>
         </div>
         <span slot="optionalIcon">
-          <i class="fas fa-heart empty-heart"></i>
+          <router-link v-if="isLoggedUser" :to="`/item/edit/${currItem._id}`">
+            <i class="far fa-edit"/>
+          </router-link>
+          <i v-else-if="loggedUser" class="fas fa-heart empty-heart"></i>
         </span>
       </garage-header>
 
       <section class="item-content">
         <div class="img-container">
-          <div v-if="isLoggedUser">
-            <button @click="removeItem">Remove</button>
-            <router-link :to="`/item/edit/${currItem._id}`"><i class="far fa-edit"/></router-link>
-          </div>
           <img class="item-img" :src="currItem.img" alt="placeholder image">
         </div>
-        <div v-if="isLoggedUser" class="chatLink-container">
-          <router-link :to="'/chat/user/'+ currItem.sellerId">&#9993;</router-link>
-          <p>Like it? Start chat</p>
-        </div>
         <div class="details-container">
-          <h3>{{currItem.title}}</h3>
+          <h3>
+            {{currItem.title}}
+            <span slot="optionalIcon">
+              <router-link v-if="isLoggedUser" :to="`/item/edit/${currItem._id}`">
+                <i class="far fa-edit"/>
+              </router-link>
+              <i v-else-if="loggedUser" class="fas fa-heart empty-heart"></i>
+            </span>
+            <span id="item-create">Listed : {{currItem.createAt | relativeTime}}</span>
+          </h3>
+          <h3>{{currItem.price}}$</h3>
           <p class="item-desc">{{currItem.desc}}</p>
-          <p><i class="fas fa-map-marker-alt"/> {{distance}} Km away</p>
+          <p>
+            <i class="fas fa-map-marker-alt"/>
+            {{distance}} Km away
+          </p>
           <p>Condition: {{currItem.condition}}</p>
-          <div class="seller-details">
-            <img class="seller-img" :src="currSeller.img" alt="placeholder image">
-            <div class="seller-details-text flex column between">
-              <p>{{currSeller.nickname}}</p>
-              <p>
-                <span v-for="n in currSeller.rate" :key="n" class="fa fa-star checked"></span>
-                <span v-for="m in (5-currSeller.rate)" :key="m.num" class="fa fa-star"></span>
-              </p>
-
-              <p>Currently selling {{currSeller.itemList.length}} items</p>
-            </div>
+          <div v-if="loggedUser" class="chatLink-container">
+            <p class="catch-dibs">
+              Like it? Dibs it!
+              <button @click="sendDibs">
+                <img class="home-logo" src="../assets/img/logo1.svg">
+              </button>
+            </p>
           </div>
+          <footer class="seller-details">
+            <div class="flex">
+              <img class="seller-img" :src="currSeller.img" alt="placeholder image">
+              <div class="seller-details-text flex column between">
+                <router-link :to="'/user/' + currSeller._id">
+                  <p>{{currSeller.nickname}}</p>
+                </router-link>
+                <p>
+                  <span v-for="n in currSeller.rate" :key="n" class="fa fa-star checked"></span>
+                  <span v-for="m in (5-currSeller.rate)" :key="m.num" class="fa fa-star"></span>
+                </p>
+                <p>Currently selling {{currSeller.itemList.length}} items</p>
+              </div>
+            </div>
+            <div class="remove-btn-container" v-if="isLoggedUser">
+              <button @click="removeItem">Remove Item</button>
+            </div>
+          </footer>
           <!-- <google-map/> -->
         </div>
       </section>
@@ -82,6 +104,22 @@ export default {
     });
   },
   methods: {
+    sendDibs() {
+      var item = this.currItem;
+      delete item.user;
+      this.$store.dispatch({
+        type: "sendDibs",
+        userId: this.loggedUser._id,
+        item
+      });
+      var user = JSON.parse(JSON.stringify(this.loggedUser));
+      user.dibsAns.unshift({
+        isAns: false,
+        item
+      });
+
+      this.$store.dispatch({ type: "updateUser", user });
+    },
     removeItem() {
       var item = this.currItem;
       this.$store.dispatch({ type: "removeItem", item }).then(() => {
@@ -127,6 +165,9 @@ export default {
       let loggedUserId = this.$store.getters.getLoggedUser._id;
       if (loggedUserId === this.currSeller._id) return true;
       return false;
+    },
+    loggedUser() {
+      return this.$store.getters.getLoggedUser;
     }
   }
 };
