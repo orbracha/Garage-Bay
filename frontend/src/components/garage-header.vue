4e2a89d6fc18702a7a210322b748811b4b518648
Ladek <eladozy@gmail.com>
2018-12-05 11:47:47 +0200

    fixed header

--- frontend/src/components/garage-header.vue
+++ frontend/src/components/garage-header.vue
@@ -11,7 +11,7 @@
           <div class="logo-text">dibs</div>
         </router-link>
       </div>
-      
+
     </div>
     <!-- <input type="text" placeholder=" Search item"> -->
     <router-link to="/search">
@@ -19,28 +19,10 @@
     </router-link>
 
     <div class="header-icons-container">
-<<<<<<< HEAD
-      <template v-if="user">
-        <router-link class="dibs-container" to="/dibs">
-          <span v-if="user.dibs.length" class="notification">{{user.dibs.length}}</span>
-          <i class="fas fa-donate"></i>
-        </router-link>
-        <router-link class="dibs-container" to="/dibs/dibsAns">
-          <span
-            v-if="user.dibsAns.filter(ans=>ans.isAns).length"
-            class="notification"
-          >{{user.dibsAns.length}}</span>
-          <i class="fas fa-shopping-cart"></i>
-        </router-link>
-      </template>
-
-      <router-link class="header-profile-icon" v-if="user" :to="'/user/'+user._id">
-=======
       
       <div v-if="user">
         <a @click="logoutUser" class="logoutBtn">Logout</a>
         <router-link class="header-profile-icon"  :to="'/user/'+user._id">
->>>>>>> dfc1d44a5a06a0bc6412b9f1dc7efb51d52d94f2
         <i class="fas fa-user"></i>
       </router-link>
       </div>
