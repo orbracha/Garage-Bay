828717b9bf70a92aeb621636506071144778a4ce
Or Bracha <orbracha2308@gmail.com>
2018-12-05 11:55:13 +0200

    dibs-css

--- frontend/src/components/garage-header.vue
+++ frontend/src/components/garage-header.vue
@@ -19,20 +19,7 @@
     </router-link>
 
     <div class="header-icons-container">
-      <template v-if="user">
-        <router-link class="dibs-container" to="/dibs">
-          <span v-if="user.dibs.length" class="notification">{{user.dibs.length}}</span>
-          <i class="fas fa-donate"></i>
-        </router-link>
-        <router-link class="dibs-container" to="/dibs/dibsAns">
-          <span
-            v-if="user.dibsAns.filter(ans=>ans.isAns).length"
-            class="notification"
-          >{{user.dibsAns.length}}</span>
-          <i class="fas fa-shopping-cart"></i>
-        </router-link>
-      </template>
-      <router-link class="header-profile-icon" v-if="user" :to="'/user/'+user._id">
+      <!-- <router-link class="header-profile-icon" v-if="user" :to="'/user/'+user._id"/> -->
       <div v-if="user">
         <a @click="logoutUser" class="logoutBtn">Logout</a>
         <router-link class="header-profile-icon"  :to="'/user/'+user._id">
